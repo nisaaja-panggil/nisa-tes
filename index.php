378753b8071045a56dd687cc355e2ab9466abbef
nisaaja-panggil <nisa31102007@gmail.com>
2024-03-06 14:41:56 +0700

    modifikasi dikit

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>haloo haloo</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus officia iusto, fugiat molestiae nobis aut officiis blanditiis delectus! Doloremque voluptate corporis mollitia ut quo corrupti aliquam nisi aperiam provident eligendi.</p>
+    <ul>
+        <li>satu aja</li>
+        <li>dua aja</li>
+    </ul>
 </body>
 </html>
